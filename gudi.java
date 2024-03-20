2d0826bbe1976f69ed8e3d71c1bb0a134ac26c35
gurusai1gudi <159562832+gurusai1gudi@users.noreply.github.com>
2024-03-20 16:48:58 +0530

    sv.java

---
+++ gudi.java
@@ -0,0 +1,10 @@
+public class gudi {
+    public static void main(String[] args) {
+        System.out.print("hello world");
+
+
+        // Adding a new line of code
+        // This line prints a message indicating the end of the program
+        System.out.println("End of program.");
+    }
+}
